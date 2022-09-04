a4755d7fdeb40c37c7c35864d05b202192e279b8
Arshya Garg <80321417+arshya-garg@users.noreply.github.com>
2022-09-04 20:07:00 +0530

    Update Q7.cpp

--- Assignment 3/Q7.cpp
+++ Assignment 3/Q7.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct students{
+        int rollno;
+        char name[30];
+    };
+    
+int main()
+{
+    struct students *s1;
+    s1 = (struct students *)malloc(sizeof(struct students));
+    s1->rollno=1;
+    strncpy(s1->name, "arshya", sizeof(s1));
+    //s1->name="arshya";
+    //s1={2,"arshya"};
+    cout<<s1->rollno<<endl;
+    cout<<s1->name<<endl;
+    return 0;
+}
 
