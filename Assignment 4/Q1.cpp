13f950604eedef8bd5da1836ddaee9b8aaabd229
Arshya Garg <80321417+arshya-garg@users.noreply.github.com>
2022-09-05 00:51:38 +0530

    Update Q1.cpp

--- Assignment 4/Q1.cpp
+++ Assignment 4/Q1.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+class student{
+    public:
+    int rollno;
+    string name;
+    
+};
+
+int main()
+{
+    student s1,s2;
+    //s1(1,"arshya");
+    //s2(2,"atharv");
+    s1.rollno=1;s1.name="Arshya";
+    s2.rollno=2;s2.name="Atharv";
+    cout<<s1.rollno<<". "<<s1.name<<endl;
+    cout<<s2.rollno<<". "<<s2.name<<endl;
+    
+
+    return 0;
+}
