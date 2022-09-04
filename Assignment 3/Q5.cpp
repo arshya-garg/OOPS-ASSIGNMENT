8cbd03ca50e33db118bcac0e2358956cb4a8f9ca
Arshya Garg <80321417+arshya-garg@users.noreply.github.com>
2022-09-04 22:35:52 +0530

    Update Q5.cpp

--- Assignment 3/Q5.cpp
+++ Assignment 3/Q5.cpp
@@ -1,1 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct cricketer{
+    char name[100];
+    int age;
+    int noOfTestMatches;
+    int avgRuns;
+    
+};
+bool compare(cricketer a,cricketer b)
+{if(a.avgRuns < b.avgRuns) {return 1;}
+else{return 0;}
+}
+
+int main()
+{
+    struct cricketer c[20];
+    int i;
+    for(i=0;i<20;i++){
+        cout<<"for "<<i+1<<"st player:";
+        cin>>c[i].name>>c[i].age>>c[i].noOfTestMatches>>c[i].avgRuns;
+}
+  int n = sizeof(c) / sizeof(c[0]);
+  sort(c, c + n,compare);
+  
+  cout<<"Ascending order:"<<endl;
+  for (int i = 0; i < 20; ++i)
+        {cout <<c[i].name<<" "<<c[i].age<<" "<<c[i].noOfTestMatches<<" "<<c[i].avgRuns<<endl;}
+    return 0;
+}
+
+
 
