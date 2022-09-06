e755e4515d5db55ce5cb5d4e6cee91e8a8b9e556
Arshya Garg <80321417+arshya-garg@users.noreply.github.com>
2022-09-07 00:50:17 +0530

    Update Q5.cpp

--- Assignment 4/Q5.cpp
+++ Assignment 4/Q5.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+class Distance{
+    int feet;
+    float inches;
+    
+    public:
+    void set(int feet,float inches){
+        this->feet=feet;
+        this->inches=inches;
+    }
+    void disp(){
+        cout<<this->feet<<"ft "<<this->inches<<"inches"<<endl;   }
+    Distance add(Distance other){
+        Distance result;
+        result.feet=feet+other.feet;
+        result.inches=inches+other.inches;
+        return result;
+        
+    }
+    
+};
+
+int main(){
+    Distance d1,d2,result;
+    d1.set(2,4.5);
+    d2.set(3,5.5);
+    d1.disp();
+    d2.disp();
+    result=d1.add(d2);
+    result.disp();
+}
